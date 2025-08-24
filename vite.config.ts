c183eace1bcb6b318ffc8fcbdfec1bfce3d0b563
Jaikumar96 <137675517+Jaikumar96@users.noreply.github.com>
2025-08-24 11:19:33 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base : "/HostSite",
+  base : "/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
